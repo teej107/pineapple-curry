4eeab1ebf8356a58ce9da8fd08681da8cc9bb6cb
teej107 <teej107@gmail.com>
2017-01-21 13:42:54 -0700

    chars to keys

--- js/advanced/chars-to-keys.js
+++ js/advanced/chars-to-keys.js
@@ -1,1 +1,18 @@
 // characters to phone keys
+var abc = "abcdefghijklmnopqrstuvwxyz";
+function charToPhoneKey(char)
+{
+    if(char.length !== 1)
+    {
+        var numbs = [];
+        for(var i = 0; i < char.length; i++)
+        {
+            numbs.push(charToPhoneKey(char.charAt(i)));
+        }
+        return numbs;
+    }
+    var index = abc.indexOf(char.toLowerCase()) + 1;
+    if (index < 1)
+        return null;
+    return Math.ceil(index / 3);
+}
